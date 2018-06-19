048851ec7525f5f5b5a937aaa2927702e14d548a
DQuent <delanouquentin@gmail.com>
2018-06-19 21:12:33 +0200

    finission persistance

--- mindstorm_project/src/main/java/domain/LumierePackage/Interrupteur.java
+++ mindstorm_project/src/main/java/domain/LumierePackage/Interrupteur.java
@@ -4,22 +4,27 @@ import lejos.nxt.ADSensorPort;
 import lejos.nxt.TouchSensor;
 import main.java.service.SysControlDLC;
 
-public class Interrupteur extends Thread{
+public class Interrupteur extends/* Thread*/TouchSensor{
+
+	public Interrupteur(ADSensorPort port) {
+		super(port);
+		// TODO Auto-generated constructor stub
+	}
 
 	private TouchSensor interrupt;
 	private boolean io = false;
 
-	public void ioActive() {
+	/*public void ioActive() {
 	   io = true;
-	}
+	}*/
 	
-	public Interrupteur(ADSensorPort port) {
+	/*public Interrupteur(ADSensorPort port) {
 		interrupt = new TouchSensor(port);
-	}
+		super(port);
+	}*/
 	
-	 public void run() {
-		 if(this.interrupt.isPressed()) {
-		 }
-	 }
+	public boolean pushed() {
+		return this.isPressed();
+	}
 
 }
--- mindstorm_project/src/main/java/service/SysControlDLC.java
+++ mindstorm_project/src/main/java/service/SysControlDLC.java
@@ -5,6 +5,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.xml.parsers.ParserConfigurationException;
+
 import lejos.nxt.ADSensorPort;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
@@ -17,12 +19,14 @@ import main.java.domain.LumierePackage.Interrupteur;
 import main.java.domain.LumierePackage.SimulateurLum;
 import main.java.infrastructure.ConfigCour;
 import main.java.infrastructure.ConfigStandard;
+import main.java.infrastructure.XMLParser;
 import main.java.utils.Minuteur;
+import java.lang.*;
 
 public class SysControlDLC {
-	private int T1;
-	private int T2;
-	private int T3;
+	private String T1;
+	private String T2;
+	private String T3;
 	private ArrayList<SimulateurLum> listeSimulateurDeLumiere;
 	private CaptMouv capteurMouvement;
 	private CaptLum capteurLumiere;
@@ -30,38 +34,42 @@ public class SysControlDLC {
 	private ConfigCour configCourante;
 	private ConfigStandard configStandard;
 	private Interrupteur interStop;
-	private Interrupteur interStart;
+	//entrées de la brique
 	private ADSensorPort p1 = SensorPort.S1;
 	private ADSensorPort p2 = SensorPort.S2;
 	private ADSensorPort p3 = SensorPort.S3 ;
 	private ADSensorPort p4 = SensorPort.S4;
+	private XMLParser xml; //toute interraction avec le config.XML passera par xml
 	
 	
-	public SysControlDLC() {
-		this.capteurLumiere = new CaptLum(this.p1);
-		this.capteurMouvement = new CaptMouv(this.p2);
-		this.interStop = new Interrupteur(p3);
+	public SysControlDLC() throws ParserConfigurationException {
+		this.capteurLumiere = new CaptLum(this.p1); //association capteur au port 1 de la brique
+		this.capteurMouvement = new CaptMouv(this.p2);//association capteur au port 2 de la brique
+		this.interStop = new Interrupteur(p3); //interrupteur au port 3 de la brique
+		//initialisation des valeurs par rapport au xml
+		this.xml = new XMLParser();
+		this.T1 = this.xml.getT1();
+		this.T2 = this.xml.getT2();
+		this.T3 = this.xml.getT3();
+		this.configCourante = this.xml.getconfCourante();
+		this.configStandard = this.xml.getconfStandard();
 	}
 	
-	public void startSystem() throws IOException, InterruptedException {
-		this.startConnectionBT();
-		while(true) {
-			if(this.capteurMouvement.detectionMouv()) {
-				LCD.drawString(Float.toString(this.capteurLumiere.detectionLum()), 0, 0);
-			}		
-		}
+	public void startSystem() throws Exception {
+		this.startConnectionBT(); //+comportement de la brique(message reçu)
+
 	}
 	
 	public void allumerLumiere() {
 		this.listeSimulateurDeLumiere.get(0).allumer();
 	}
 	
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
     	SysControlDLC sys = new SysControlDLC();
     	sys.startSystem();
     }
     
-	public void startConnectionBT() throws IOException, InterruptedException {
+	public void startConnectionBT() throws Exception {
 		boolean isrunning = true;
 
 		// Main loop   
@@ -83,27 +91,146 @@ public class SysControlDLC {
 
 		  // The InputStream for read data 
 		  DataInputStream dis = btc.openDataInputStream();
-		  //DataOutputStream out = btc.openDataOutputStream();
-		  LCD.clear();
-		  LCD.drawString("test", 1, 1);
-		  LCD.refresh(); 
-		LCD.clear(); 
+		  DataOutputStream out = btc.openDataOutputStream();
+		  
 		// Loop for read data  
 		  while (isrunning) {
 	
-		    String n = dis.readUTF();
-			  //byte n[] = dis.readByte();
+		    String msg = dis.readUTF();
+			  //byte msg[] = dis.readByte();
  
-		    LCD.drawString(n, 4, 4);
-
-		  //  LCD.drawInt(n, 1, 1);
-			//out.writeUTF("coucou théo");;
-			//out.flush();
+		    LCD.drawString(msg, 4, 4);
+			
+			//quelqu'un rentre dans la piece ?
+			if(this.capteurMouvement.detectionMouv()) {
+				LCD.drawString(Float.toString(this.capteurLumiere.detectionLum()), 0, 0);
+			}		
+			/*TODO : Evenement en fonction du message reçu (msg)*/
+			//modif T1 : exemple de condition par msg reçu
+			if(msg.indexOf("setT1") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//créer l'objet en question (ici juste une valeur pour T)
+				// ajouter/modifier le xml : (il faut voir les méthodes dans XMLParser )
+				this.xml.save(); //ecriture de la modification dans le xml
+			}	
+			//modif T2
+			if(msg.indexOf("setT2")> -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//créer l'objet en question 
+				// ajouter/modifier le xml : (il faut voir les méthodes dans XMLParser )
+				this.xml.save(); //ecriture de la modification dans le xml
+			}
+			//modif T3
+			if(msg.indexOf("setT3")> -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//créer l'objet en question 
+				// ajouter/modifier le xml : (il faut voir les méthodes dans XMLParser )
+				this.xml.save(); //ecriture de la modification dans le xml
+			}
+			//modif ConfigStandard
+			if(msg.indexOf("setConfigStandard") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//créer l'objet en question 
+				// ajouter/modifier le xml : (il faut voir les méthodes dans XMLParser )
+				this.xml.save(); //ecriture de la modification dans le xml
+			}
+			//modif ConfigCourante
+			if(msg.indexOf("setConfigCour") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//créer l'objet en question 
+				// ajouter/modifier le xml : (il faut voir les méthodes dans XMLParser )
+				this.xml.save(); //ecriture de la modification dans le xml
+			}
+			//envoyer T1 au smartphone
+			if(msg.indexOf("sendT1") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//récupérer l'objet en question à l'aide du parser
+				//out.writeUTF(); envoie de l'objet (faire une string du type <object/action>;<attribut1>;...;<attributN> :  T1;4 ou ConfigStandard;1;conf1;lum1)
+				out.flush();
+			}
+			//envoyer T2 au smartphone
+			if(msg.indexOf("sendT2") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//récupérer l'objet en question à l'aide du parser
+				//out.writeUTF(); envoie de l'objet (faire une string du type <object/action>;<attribut1>;...;<attributN> :  T1;4 ou ConfigStandard;1;conf1;lum1)
+				out.flush();
+			}
+			
+			//envoyer T3 au smartphone
+			if(msg.indexOf("sendT3") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//récupérer l'objet en question à l'aide du parser
+				//out.writeUTF(); envoie de l'objet (faire une string du type <object/action>;<attribut1>;...;<attributN> :  T1;4 ou ConfigStandard;1;conf1;lum1)
+				out.flush();
+			}
+			//envoyer ConfigStandard au smartphone
+			if(msg.indexOf("sendConfigStandard") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//récupérer l'objet en question à l'aide du parser
+				//out.writeUTF(); envoie de l'objet (faire une string du type <object/action>;<attribut1>;...;<attributN> :  T1;4 ou ConfigStandard;1;conf1;lum1)
+				out.flush();
+			}
+			//envoyer ConfigCourante au smartphone
+			if(msg.indexOf("sendConfigCourante") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//récupérer l'objet en question à l'aide du parser
+				//out.writeUTF(); envoie de l'objet (faire une string du type <object/action>;<attribut1>;...;<attributN> :  T1;4 ou ConfigStandard;1;conf1;lum1)
+				out.flush();
+			}
+			//envoyer toute les configAdmin(=configStandard avec isStandard= false) au smartphone
+			if(msg.indexOf("sendAllConf") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//récupérer l'objet en question à l'aide du parser
+				//out.writeUTF(); envoie de l'objet (faire une string du type <object/action>;<attribut1>;...;<attributN> :  T1;4 ou ConfigStandard;1;conf1;lum1)
+				out.flush();
+			}
+			//envoyer toute les lumieres au smartphone
+			if(msg.indexOf("sendAllLum") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//récupérer l'objet en question à l'aide du parser
+				//out.writeUTF(); envoie de l'objet (faire une string du type <object/action>;<attribut1>;...;<attributN> :  T1;4 ou ConfigStandard;1;conf1;lum1) 
+				// POUR TOUTES LES LUMIERES FAIRE ATTENTION A LA LONGUEUR DE CHAINE, peut être ne renvoyer que les 3 premières ?
+				out.flush();
+			}
+			//allumer une lumiere
+			if(msg.indexOf("on") > -1) {
+				//découper le msg : faire regex pour découper la string par ";", du type "on;l" pour allumer lumoière ayant id=1
+			}
+			//ajouter une lumiere à une configuration
+			if(msg.indexOf("addLumToConf") > -1) {
+				//découper le msg : faire regex pour découper la string par ";" ==> "addLumToConf;<lum>;<conf>"
+				//ajouter l'objet en question à l'aide du parser
+			}
+			//ajouter une lumière au systeme
+			if(msg.indexOf("addLum") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//ajouter l'objet en question à l'aide du parser
+			}
+			//ajouter une configuration Admin (la confAdmin est une configStandard avec isStandard= false)
+			if(msg.indexOf("addConf") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//ajouter l'objet en question à l'aide du parser
+			}
+			//supprimer une lumière au systeme
+			if(msg.indexOf("deleteLum") > -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//ajouter l'objet en question à l'aide du parser
+			}
+			//ajouter une configuration Admin (la confAdmin est une configStandard avec isStandard= false)
+			if(msg.indexOf("deleteConf")> -1) {
+				//découper le msg : faire regex pour découper la string par ";"
+				//ajouter l'objet en question à l'aide du parser
+			}
+			//Evenement extinction de la brique : pression sur interrupteur du port 3 de la brique
+			if(this.interStop.pushed()) {
+				isrunning = false;
+			}
+			/*FIN TODO*/
 			LCD.refresh();
 		  }
 	
 		  dis.close();
-		  //out.close();
+		  out.close();
 		  // Wait for data to drain
 			Thread.sleep(100);
 
